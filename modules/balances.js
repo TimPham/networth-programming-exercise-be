db85e907460db01f1fb6e2b7a0417531ef1ffdeb
Timothy Pham <TimPham@users.noreply.github.com>
2021-10-08 18:25:47 -0400

    Updated balances module to calculate CAD, BTC and ETH balances.

--- modules/balances.js
+++ modules/balances.js
@@ -26,14 +26,95 @@ const loadRemoteFeed = async (url) => {
 }
 
 const process = async (data) => {
-    // TODO: Calculate fiat + crypto balances
-    return {
+    // Keep track of all of the balances
+    let balances = {
         current_balance_cad: 0,
         current_balance_btc: 0,
         current_balance_eth: 0,
-        number_transactions: data.length,
-        transaction_data: {},
     }
+
+    // Check if there are records to process
+    if (data.length > 0) {
+        // TODO: Calculate historical balance over time
+        await data.forEach((record) => {
+            if (typeof record.type !== 'undefined' && typeof record.currency !== 'undefined') {
+                switch (record.type) {
+                    case 'debit':
+                        // Withdrawal
+                        if (record.amount > 0) {
+                            switch (record.currency) {
+                                case 'CAD':
+                                    balances.current_balance_cad -= record.amount
+                                    break;
+                                case 'BTC':
+                                    balances.current_balance_btc -= record.amount
+                                    break;
+                                case 'ETH':
+                                    balances.current_balance_eth -= record.amount
+                                    break;
+                            }
+                        }
+                        break;
+                    case 'credit':
+                        // Deposit
+                        if (record.amount > 0) {
+                            switch (record.currency) {
+                                case 'CAD':
+                                    balances.current_balance_cad += record.amount
+                                    break;
+                                case 'BTC':
+                                    balances.current_balance_btc += record.amount
+                                    break;
+                                case 'ETH':
+                                    balances.current_balance_eth += record.amount
+                                    break;
+                            }
+                        }
+                        break;
+                    case 'conversion':
+                        if (typeof record.from !== 'undefined' && typeof record.to !== 'undefined') {
+                            if (typeof record.from.currency !== 'undefined' && typeof record.to.currency !== 'undefined') {
+                                // Adjust from
+                                switch (record.from.currency) {
+                                    case 'CAD':
+                                        balances.current_balance_cad -= record.from.amount
+                                        break;
+                                    case 'BTC':
+                                        balances.current_balance_btc -= record.from.amount
+                                        break;
+                                    case 'ETH':
+                                        balances.current_balance_eth -= record.from.amount
+                                        break;
+                                }
+
+                                // Adjust to
+                                switch (record.to.currency) {
+                                    case 'CAD':
+                                        balances.current_balance_cad += record.to.amount
+                                        break;
+                                    case 'BTC':
+                                        balances.current_balance_btc += record.to.amount
+                                        break;
+                                    case 'ETH':
+                                        balances.current_balance_eth += record.to.amount
+                                        break;
+                                }
+                            }
+                        }
+                        break;
+                }
+            }
+        })
+    }
+
+    // TODO: Calculate fiat + crypto balances
+    return Object.assign(
+        balances,
+        {
+            number_transactions: data.length,
+            transaction_data: {},
+        }
+    )
 }
 
 /**
@@ -45,12 +126,12 @@ const process = async (data) => {
  */
 const processLocalFile = async (filepath) => {
     const feedData = await loadLocalFile(filepath)
-    return process(feedData)
+    return await process(feedData)
 }
 
 const processRemoteFeed = async (url) => {
     const feedData = await loadRemoteFeed(url)
-    return process(feedData)
+    return await process(feedData)
 }
 
 // Export Server configuration
